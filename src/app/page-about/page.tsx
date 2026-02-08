683f9c8c2b094d218549088af715c275b1e757e6
Abu Musa <96811902+mdabumusasat@users.noreply.github.com>
2026-02-08 12:08:47 +0600

    update

--- src/app/page-about/page.tsx
+++ src/app/page-about/page.tsx
@@ -4,7 +4,7 @@ import LayoutWrapper from "../LayoutWrapper";
 import PageTitle from "../components/PageTitle";
 import About from "../section/home1/About";
 import Success from "../section/home3/Success";
-import Services from "../section/about/Services";
+import Services from "../section/About/Services";
 import Testimonial from "../section/home1/Testimonial";
 
 export const metadata = {
